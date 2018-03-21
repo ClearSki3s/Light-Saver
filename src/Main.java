e105a714aaae0d504e5bbb590001f8fc0e1c9396
Thomas Brouwers <tmbrouwers@gmail.com>
2018-03-21 15:23:31 +0100

    write to lamp works

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import HUELampControl.HUELamp;
+import HUELampControl.JsonConnect;
 import WeatherstationControl.Measurement;
 import WeatherstationControl.RawMeasurement;
 import WeatherstationControl.WeatherStation;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,18 +14,23 @@ import java.util.Iterator;
 
 import static HUELampControl.JsonConnect.readJsonFromUrl;
 
+
 /**
  * Created by Trist on 21-3-2018.
  */
 public class Main {
     public static void main(String[] args) {
-
+        Gson gson = new Gson();
+        JsonConnect jsonConnect = new JsonConnect();
         String url = "http://145.48.205.33/api/iYrmsQq1wu5FxF9CPqpJCnm1GpPVylKBWDUsNDhB/lights";
         ArrayList<HUELamp> lampen;
-
+        JsonObject json;
+        String jsonString;
+        JsonObject response;
+        JsonParser parser = new JsonParser();
         lampen = new ArrayList<>();
         try {
-            JsonObject response = readJsonFromUrl(url);
+            response = readJsonFromUrl(url);
             Iterator<String> iterator = response.keySet().iterator();
             while(iterator.hasNext()) {
                 String key = iterator.next();
@@ -32,7 +41,10 @@ public class Main {
         }
         for(int i = 0; i< lampen.size(); i++)
         {
-            System.out.println(lampen.get(i).getName());
+            //System.out.println(lampen.get(i).getName());
+            lampen.get(i).setBri(254);
+            lampen.get(i).setHue(254);
+            lampen.get(i).setSat(43690);
         }
 
         WeatherStation ws = new WeatherStation();
@@ -45,6 +57,25 @@ public class Main {
         System.out.println(measurement.calcTemperature(ws.getMostRecentOutsideTemp()));
         System.out.println(measurement.calcRainFall(ws.getMostRecentRainRate()));
         System.out.println(measurement.calcWindSpeed(ws.getMostRecentWindSpeed()));
+
+        for(int i = 0; i< lampen.size(); i++) {
+            json = new JsonObject();
+            url = "http://145.48.205.33/api/iYrmsQq1wu5FxF9CPqpJCnm1GpPVylKBWDUsNDhB/lights/" + lampen.get(i).getId() + "/state";
+            json.addProperty("on", true);
+            json.addProperty("bri", 254);
+            json.addProperty("hue", 43690);
+            json.addProperty("sat", 254);
+            System.out.println(i + ": " + json);
+
+            jsonString = json.toString();
+
+            try {
+                jsonConnect.writeJsonToUrl(url,jsonString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
